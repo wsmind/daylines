0a79ebbadf6e7c1a1e7b69c0ac46a78e95554045
Remi Papillie <remi.papillie@gmail.com>
2019-09-14 22:45:45 +0200

    wip calendar view

---
+++ src/CalendarView/CalendarView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+import Week from './Week';
+
+const CalendarView = () => (
+  <View>
+    <Week />
+    <Week />
+    <Week />
+  </View>
+);
+
+export default CalendarView;
